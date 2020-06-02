b79b622f29c95fcfc0eab3efd13bc638d838ebe5
Joab Romero <jjromeroh@eafit.edu.co>
2020-06-01 23:59:18 -0500

    "cambio v4"

--- luminosity/views.py
+++ luminosity/views.py
@@ -6,6 +6,7 @@ from matplotlib import pyplot
 import io
 #from django.http import HttpResponse
 from matplotlib.backends.backend_agg import FigureCanvasAgg
+from random import sample
 
 #import request
 # Create your views here.
@@ -78,4 +79,36 @@ def grafico():
 
     response = HttpResponse(buf.getvalue(), content_type='image/png')
     response['Content-Length'] = str(len(response.content))
+    return response
+
+def plot(request):
+    # Creamos los datos para representar en el gráfico
+    x = range(1,11)
+    y = sample(range(20), len(x))
+
+    # Creamos una figura y le dibujamos el gráfico
+    f = pyplot.figure()
+
+    # Creamos los ejes
+    axes = f.add_axes([0.15, 0.15, 0.75, 0.75]) # [left, bottom, width, height]
+    axes.plot(x, y)
+    axes.set_xlabel("Eje X")
+    axes.set_ylabel("Eje Y")
+    axes.set_title("Mi gráfico dinámico")
+
+    # Como enviaremos la imagen en bytes la guardaremos en un buffer
+    buf = io.BytesIO()
+    canvas = FigureCanvasAgg(f)
+    canvas.print_png(buf)
+
+    # Creamos la respuesta enviando los bytes en tipo imagen png
+    response = HttpResponse(buf.getvalue(), content_type='image/png')
+
+    # Limpiamos la figura para liberar memoria
+    f.clear()
+
+    # Añadimos la cabecera de longitud de fichero para más estabilidad
+    response['Content-Length'] = str(len(response.content))
+
+    # Devolvemos la response
     return response
